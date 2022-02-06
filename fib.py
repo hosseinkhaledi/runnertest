0944ca736d50674dd521b84b9148040a180e5a3a
HosseiN Khademi Khaledi <hossein.khaledi@yahoo.com>
2022-02-06 05:21:36 +0330

    Rename Action to Unit Test

---
+++ fib.py
@@ -0,0 +1,5 @@
+"""Finbonachi"""
+
+
+def fib(n: int) -> int:
+    return n if n < 2 else fib(n - 1) + fib(n - 2)
--- test/test_fib.py
+++ test/test_fib.py
@@ -1,8 +1,11 @@
 # testing Fibonacci number function
 # pylint: skip-file
-def fib(n: int) -> int:
-    return n if n < 2 else fib(n - 1) + fib(n - 2)
+from fib import fib
 
 
 def test_fibonacci():
     assert fib(10) == 55
+
+
+def test_hamed():
+    assert fib(1) == 1
